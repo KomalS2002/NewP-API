8d75df4b6b02a4a57e80065e7f9884c76fa6299f
KomalS2002 <komalsingh10072002@gmail.com>
2023-06-06 14:33:34 +0530

    routes for follow,unfollow,post-store added

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
 const postRoute = require("./routes/posts")
+const multer = require("multer");
+const path = require("path")
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true,useUnifiedTopology:true}, ()=>{
     console.log("connected to mongo");
 });
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.listen(8100, ()=>{
     console.log("Hello komal welcome on your first backend server on port 8100");
@@ -20,8 +23,27 @@ app.listen(8100, ()=>{
 
 // middlewares
 app.use(express.json());
-app.use(helmet());
+app.use(helmet({ crossOriginResourcePolicy: false, }));
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb) =>{
+    cb(null,"public/images/");
+    },
+    filename: (req,file,cb) =>{
+    cb(null,req.body.name);
+    },
+
+});
+
+const upload = multer({storage});
+app.post("/api/upload", upload.single("file"), (req,res)=>{
+    try{
+    res.status(200).json("file uploaded successfully")
+    }catch(err){
+        console.log(err);
+    }
+})
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
--- routes/users.js
+++ routes/users.js
@@ -40,12 +40,36 @@ router.delete("/:id", async(req,res)=>{
 })
 
 //get user
-router.get("/:id", async(req,res)=>{
+router.get("/", async(req,res)=>{
+   const userId = req.query.userId;
+   const username = req.query.username;
    try{
-   const user = await User.findById(req.params.id)
+   const user = userId
+   ? await User.findById(userId)
+   : await User.findOne({username:username});
    const{password, updatedAt,...other} = user._doc
    res.status(200).json(other);
    }catch(err){console.log(err);}
+})
+
+//get friend
+router.get("/friends/:userId", async(req,res)=>{
+   try{
+   const user = await User.findById(req.params.userId);
+   const friends = await Promise.all(
+      user.following.map(friendId =>{
+         return User.findById(friendId)
+      })
+   )
+   let friendList = [];
+   friends.map((friend) =>{
+      const {_id, username, profilePicture}= friend;
+      friendList.push({_id, username, profilePicture});
+   });
+   res.status(200).json(friendList);
+   }catch(err){
+      console.log(err)
+   }
 })
 
 //follow other user
